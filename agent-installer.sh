7b23b60306aa19d45f9703c78abfaea20989d7e2
iyuroch <iyuroch@gmail.com>
2018-01-04 13:25:43 +0200

    Minor bugs, added install/uninstall

--- agent-installer.sh
+++ agent-installer.sh
@@ -7,8 +7,21 @@ SCRIPTLOCATION="/var/lib/" #folder where script will persist
 PLISTTNAME="com.example.log-example.plist" #name of plist file
 PLISTLOCATION="~/Library/LaunchAgents/" #location of plist file
 
+function install() {
+if [ ! -d "$PLISTLOCATION" ]; then
+	mkdir $PLISTLOCATION
+fi
+
+chmod 700 $PLISTNAME
 chmod 700 $SCRIPTNAME
-mv $PLISTNAME $PLISTLOCATION
-mv $SCRIPTNAME $SCRIPTLOCATION
-mv /var/lib/log-example.sh
-launchctl load -w $PLISTNAME$PLISTLOCATION
+
+cp $PLISTNAME $PLISTLOCATION
+cp $SCRIPTNAME $SCRIPTLOCATION
+
+launchctl load -w $PLISTLOCATION$PLISTNAME
+}
+
+function uninstall() {
+rm $PLISTLOCATION$PLISTNAME
+rm $SCRIPTLOCATION$SCRIPTNAME
+}
